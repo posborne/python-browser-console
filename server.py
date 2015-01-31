e50ea44e9d94eddc31695371a3353a1f7292bebd
Paul Osborne <osbpau@gmail.com>
2015-01-31 16:34:35 -0600

    core: push down and render based on records rather than pushing text
    This provides more flexibility as far as how input/output lines are
    displayed on the client.  It's better.

--- server.py
+++ server.py
@@ -14,9 +14,10 @@ def post_to_console():
         console.push(inp)
 
     return json.dumps({
-        "output": cgi.escape(console.get_output()),
-        "history": console.get_history()
+        "outputRecords": list(console.get_output_lines()),
+        "history": console.get_history(),
     })
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
